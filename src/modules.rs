e375660825942a69f87c3738bc711311ba929342
guobin <guobin@bituniverse.org>
2025-11-21 23:54:47 +0800

    qr and cam

--- src/modules.rs
+++ src/modules.rs
@@ -32,6 +32,8 @@ mod time;
 mod unicode;
 mod url;
 mod usage;
+mod qr;
+mod cam;
 
 #[derive(Clone)]
 pub struct Module<'a, 'b> {
@@ -85,6 +87,8 @@ impl<'a, 'b> ModuleManager<'a, 'b> {
 		mm.register(sm4::module());
 		mm.register(eddsa::module());
 		mm.register(srdsa::module());
+		mm.register(qr::module());
+		mm.register(cam::module());
 		mm
 	}
 
